75ee9efb9a715bed64005ca08e5c54eba7c6af2c
exitoso <korotae-v@yandex.ru>
2013-03-17 17:53:43 +0400

    Fix bag: wrong location tag "negos"

--- list_of_universities/listOfUniversities.php
+++ list_of_universities/listOfUniversities.php
@@ -24,7 +24,7 @@ $rootNode = $dom->appendChild(new DOMElement('root'));
 foreach($formOfIncorporation as $typeIncorporation) {
 
     $incorporation = new DOMElement($typeIncorporation);
-    $rootNode = $rootNode->appendChild($incorporation);
+    $rootNode->appendChild($incorporation);
 
     foreach($dataRegions as $key=>$value) {
 
